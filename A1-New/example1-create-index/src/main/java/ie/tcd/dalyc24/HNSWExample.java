86fc211b28f02e51715e9c5976c4bcb575b85115
lasers67 <pratyushgauravgo@gmail.com>
2024-11-08 07:27:52 +0000

    mc

--- A1-New/example1-create-index/src/main/java/ie/tcd/dalyc24/HNSWExample.java
+++ A1-New/example1-create-index/src/main/java/ie/tcd/dalyc24/HNSWExample.java
@@ -1,4 +1,5 @@
 package ie.tcd.dalyc24;
+
 import org.apache.lucene.index.VectorEncoding;
 import org.apache.lucene.index.VectorSimilarityFunction;
 import org.apache.lucene.document.Document;
@@ -18,18 +19,20 @@ import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 import org.apache.lucene.util.hnsw.HnswGraphBuilder;
 import org.apache.lucene.util.hnsw.HnswGraphSearcher;
 import ie.tcd.dalyc24.ListRandomAccessVectorValues;
 
-
 public class HNSWExample {
     private static final int VECTOR_DIMENSION = 3072;
     private Directory directory;
     private static final VectorSimilarityFunction similarityFunction = VectorSimilarityFunction.COSINE;
-    private List<String> resultsFile = new ArrayList<String>();
-    private int num=1;
+    private List<String> resultsFile = new ArrayList<>();
+    private int num = 1;
+
     public HNSWExample() throws Exception {
         directory = FSDirectory.open(Paths.get("./index/"));
     }
@@ -47,7 +50,6 @@ public class HNSWExample {
                 }
                 rows.add(row);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -57,20 +59,21 @@ public class HNSWExample {
     }
 
     public void search(List<float[]> universe, float[] queryVector, int k) throws Exception {
-        //List<float[]> universeList = new ArrayList<>(List.of(universe));
-        // Ensure HnswGraphBuilder and HnswGraphSearcher are properly defined or imported.
         var ravv = new ListRandomAccessVectorValues(universe, VECTOR_DIMENSION);
         var builder = HnswGraphBuilder.create(ravv, VectorEncoding.FLOAT32, similarityFunction, 16, 100, new Random().nextInt());
         var hnsw = builder.build(ravv.copy());
         var nn = HnswGraphSearcher.search(queryVector, k, ravv.copy(), VectorEncoding.FLOAT32, similarityFunction, hnsw, null, Integer.MAX_VALUE);
-        
-        // Placeholder for loop over nearest neighbors:
+
         for (var i : nn.nodes()) {
-             var neighbor = universe.get(i);
-             var similarity = similarityFunction.compare(queryVector, neighbor);
-             resultsFile.add(num + " Q0 "+ i + " 0 " + similarity + " STANDARD");
-             num++;
-         }
+            var neighbor = universe.get(i);
+            var similarity = similarityFunction.compare(queryVector, neighbor);
+            resultsFile.add(num + " Q0 " + i + " 0 " + similarity + " STANDARD");
+            num++;
+        }
+    }
+
+    public void writeResultsToFile() throws IOException {
+        Files.write(Paths.get("./resultsNEW.txt"), resultsFile, StandardCharsets.UTF_8);
     }
 
     public static void main(String[] args) throws Exception {
@@ -79,9 +82,11 @@ public class HNSWExample {
         String queryFileName = "../query.txt";
         List<float[]> A = read2DFloatArrayFromFile(indexFileName);
         List<float[]> queryVectors = read2DFloatArrayFromFile(queryFileName);
+
         for (float[] query : queryVectors) {
             example.search(A, query, 1000);
         }
-         Files.write(Paths.get("./resultsNEW.txt"),resultsFile,Charset.forName("UTF-8"));
+
+        example.writeResultsToFile();
     }
 }
