f65442094701c5082dba4ca73bc545a51603f1ed
lasers67 <pratyushgauravgo@gmail.com>
2024-11-10 18:50:01 +0000

    mc

--- A1-New/example1-create-index/src/main/java/ie/tcd/dalyc24/HNSWExample.java
+++ A1-New/example1-create-index/src/main/java/ie/tcd/dalyc24/HNSWExample.java
@@ -114,8 +114,12 @@ num++;
         String queryFileName = "../query.txt";
         List<float[]> A = read2DFloatArrayFromFile(indexFileName);
         List<float[]> queryVectors = read2DFloatArrayFromFile(queryFileName);
+        int counter = 0;
         for (float[] query : queryVectors) {
             example.search(A, query, 50);
+            counter++;
+            if(counter==10)
+                break;
         }
 
         example.writeResultsToFile();
