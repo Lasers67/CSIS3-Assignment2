81a269f676f9d35ff45c1d827f794deb341a8b1f
ved74thakured <thakurv@tcd.ie>
2024-11-20 03:05:53 +0000

    Changed Code

--- Code/src/main/java/ie/tcd/dalyc24/CreateIndex.java
+++ Code/src/main/java/ie/tcd/dalyc24/CreateIndex.java
@@ -13,133 +13,114 @@ import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.search.similarities.BM25Similarity;
-// import org.apache.lucene.store.RAMDirectory;
- 
-public class CreateIndex
-{
-    
-    // index Directory
-    private static String INDEX_DIRECTORY = "./index/";
-	public static void createIndexForEachFile(String analyzer_name) 
-	{
-		Analyzer analyzer = null;
-		System.out.println(analyzer_name + " For Index");
-		ProcessData proc = new ProcessData();
-		// Choose the analyzer based on the input name
-		switch (analyzer_name) {
-			case "standard":
-				analyzer = new StandardAnalyzer(EnglishAnalyzer.getDefaultStopSet());
-				break;
-			case "keyword":
-				analyzer = new KeywordAnalyzer();
-				break;
-			case "whitespace":
-				analyzer = new WhitespaceAnalyzer();
-				break;
-			case "simple":
-				analyzer = new SimpleAnalyzer();
-				break;
-			case "stop":
-				analyzer = new StopAnalyzer();
-				break;
-			case "english":
-				analyzer = new EnglishAnalyzer();
-				break;
-			default:
-				analyzer = new StandardAnalyzer();
-				System.out.println("default analyzer\n");
-				break;
-		}
 
-		String[] DATA_FOLDERS = {"latimes/", "ft/", "fr94/", "fbis/"};
-		try{
-		Directory indexDirectory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
-		IndexWriterConfig config = new IndexWriterConfig(analyzer);
-		config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND); // Always create a new index
-		IndexWriter iwriter = new IndexWriter(indexDirectory, config);
-		// Iterate over each data folder and process files
-		for (String folder : DATA_FOLDERS) {
-			File directory = new File("../Data/" + folder);
-			if (directory.isDirectory()) {
-				File[] files = directory.listFiles();
-				if (files != null && files.length > 0) {
-					for (File file : files) {
-						if (file.isFile()) {
-							if (!file.getName().startsWith("read")) {
-								try {
-									System.out.println("Directory:- " + folder + " file:- " + file.getName());
-									// Read the document from the file and add it to the index
-									ArrayList<Document> documents = proc.readFiles_Dataset_File(file.getAbsolutePath());
-									iwriter.addDocuments(documents);
-								} catch (IOException e) {
-									e.printStackTrace();
-								}
-							}
-						} else if (file.isDirectory()) {
-							// Handle nested directories
-							File[] nestedFiles = file.listFiles();
-								if (nestedFiles != null) {
-									for (File nestedFile : nestedFiles) {
-										if (nestedFile.isFile()) {
-											try {
-												System.out.println("Directory:- " + folder + " file:- " + nestedFile.getAbsolutePath());
-									// Create a new index for each file
+public class CreateIndex {
 
-									// Read the document from the file and add it to the index
-									ArrayList<Document> documents = proc.readFiles_Dataset_File(nestedFile.getAbsolutePath());
-									iwriter.addDocuments(documents);
-								} catch (IOException e) {
-									e.printStackTrace();
-								}
-										}
-									}
-								}
-						}
-					}
-				} else {
-					System.out.println("The directory " + folder + " is empty.");
-				}
-			} else {
-				System.out.println("The specified path is not a directory: " + folder);
-			}
-		}
-		iwriter.close(); // Close the writer
-		indexDirectory.close(); // Close the directory
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-    public static void deleteprevDir() {
-    try {
-        File f = new File(INDEX_DIRECTORY);
-        
-        // Check if the directory exists
-        if (f.exists()) {
-            // Clean the directory
-            FileUtils.cleanDirectory(f); // clean directory
-            FileUtils.forceDelete(f); // delete directory
+    // Directory where the Lucene index will be stored
+    private static final String INDEX_DIRECTORY = "./index/";
+
+    public static void createIndexForEachFile(String analyzerName) {
+        Analyzer analyzer = null;
+        System.out.println(analyzerName + " For Index");
+        ProcessData proc = new ProcessData();
+
+        // Choose the analyzer based on the input name
+        switch (analyzerName.toLowerCase()) {
+            case "standard":
+                analyzer = new StandardAnalyzer(EnglishAnalyzer.getDefaultStopSet());
+                break;
+            case "keyword":
+                analyzer = new KeywordAnalyzer();
+                break;
+            case "whitespace":
+                analyzer = new WhitespaceAnalyzer();
+                break;
+            case "simple":
+                analyzer = new SimpleAnalyzer();
+                break;
+            case "stop":
+                analyzer = new StopAnalyzer();
+                break;
+            case "english":
+                analyzer = new EnglishAnalyzer();
+                break;
+            default:
+                analyzer = new StandardAnalyzer();
+                System.out.println("Using default analyzer: StandardAnalyzer");
+                break;
         }
-        
-        // Create the directory
-        FileUtils.forceMkdir(f); // create directory
-    } catch (IOException e) {
-        e.printStackTrace();
+
+        // Define the dataset path
+        String DATA_DIRECTORY = "/opt/CSIS3-Assignment2/Data/Assignment Two/";
+        String[] DATA_FOLDERS = {"latimes/", "ft/", "fr94/", "fbis/"};
+
+        try {
+            // Initialize Lucene's index directory
+            Directory indexDirectory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND); // Always create or append to the index
+            IndexWriter iwriter = new IndexWriter(indexDirectory, config);
+
+            // Iterate over each data folder and process files
+            for (String folder : DATA_FOLDERS) {
+                File directory = new File(DATA_DIRECTORY + folder);
+                if (directory.isDirectory()) {
+                    File[] files = directory.listFiles();
+                    if (files != null && files.length > 0) {
+                        for (File file : files) {
+                            if (file.isFile() && !file.getName().startsWith("read")) {
+                                try {
+                                    System.out.println("Processing Directory: " + folder + ", File: " + file.getName());
+                                    // Read the document from the file and add it to the index
+                                    ArrayList<Document> documents = proc.readFiles_Dataset_File(file.getAbsolutePath());
+                                    iwriter.addDocuments(documents);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    } else {
+                        System.out.println("The directory " + folder + " is empty.");
+                    }
+                } else {
+                    System.out.println("The specified path is not a directory: " + folder);
+                }
+            }
+
+            // Close the writer and directory
+            iwriter.close();
+            indexDirectory.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
-    public static void main(String[] args) throws IOException
-    {
-	//delete the previous directory
-	deleteprevDir();
-	//create Index
-	createIndexForEachFile(args[0]);
+
+    public static void deletePreviousIndex() {
+        try {
+            File indexDir = new File(INDEX_DIRECTORY);
+
+            // Check if the directory exists
+            if (indexDir.exists()) {
+                // Clean and delete the directory
+                FileUtils.cleanDirectory(indexDir); // Clean directory
+                FileUtils.forceDelete(indexDir); // Delete directory
+            }
+
+            // Recreate the directory
+            FileUtils.forceMkdir(indexDir); // Create directory
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Delete the previous index directory
+        deletePreviousIndex();
+        // Create a new index
+        createIndexForEachFile(args[0]);
     }
 }
--- Code/src/main/java/ie/tcd/dalyc24/LuceneSearch.java
+++ Code/src/main/java/ie/tcd/dalyc24/LuceneSearch.java
@@ -1,4 +1,5 @@
 package ie.tcd.dalyc24;
+
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.io.IOException;
 
-import java.io.File;
+import java.io.FileWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import org.apache.lucene.analysis.Analyzer;
@@ -22,12 +23,9 @@ import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
 
 // BM25Similarity
 import org.apache.lucene.search.similarities.BM25Similarity;
@@ -46,8 +44,6 @@ import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
 
 // Information-Based (IB) Similarity
 import org.apache.lucene.search.similarities.IBSimilarity;
-import org.apache.lucene.search.similarities.Distribution;
-import org.apache.lucene.search.similarities.Lambda;
 import org.apache.lucene.search.similarities.DistributionLL;
 import org.apache.lucene.search.similarities.LambdaDF;
 import org.apache.lucene.search.similarities.NormalizationH1;
@@ -55,108 +51,123 @@ import org.apache.lucene.search.similarities.NormalizationH1;
 // Axiomatic Similarity
 import org.apache.lucene.search.similarities.AxiomaticF2LOG;
 
-
 public class LuceneSearch {
-	public static void main(String[] args) throws IOException {
-        	ProcessData readFile = new ProcessData();
-        	List<Map<String, String>> queryList = readFile.readQueries();
-        	LuceneSearch searcher = new LuceneSearch();
-		System.out.println(args[0] + " " + args[1]);
-        	searcher.searchQueriesInData(args[0], args[1],queryList);
-    	}
-	public static void searchQueriesInData(String analyzer_name, String similarity_strat,List<Map<String,String>> queryList)
-	{
-		try{
-			Analyzer analyzer = null;
-			//use same analyzer as Create Index class
-			switch(analyzer_name)
-        		{
-                		case "standard":
-                        		analyzer = new StandardAnalyzer(EnglishAnalyzer.getDefaultStopSet());
-                        		break;
-                		case "keyword":
-                        		analyzer = new KeywordAnalyzer();
-                        		break;
-                		case "whitespace":
-                        		analyzer = new WhitespaceAnalyzer();
-                        		break;
-                		case "simple":
-                        		analyzer = new SimpleAnalyzer();
-                        		break;
-                		case "stop":
-                        		analyzer = new StopAnalyzer();
-                        		break;
-                		case "english":
-                        		analyzer = new EnglishAnalyzer();
-                        		break;
-                		default:
-                        		analyzer = new StandardAnalyzer();
-                        		break;
-       			}
-			//load indexs
-			Directory directory = FSDirectory.open(Paths.get("./index/"));
-			DirectoryReader ireader = DirectoryReader.open(directory);
-			IndexSearcher isearcher = new IndexSearcher(ireader);
-			//depending on the sys arg choose similarity score
-			switch(similarity_strat)
-			{
-				case "classic":
-					isearcher.setSimilarity(new ClassicSimilarity());
-					break;
-				case "bm25":
-					isearcher.setSimilarity(new BM25Similarity(1.9f,1.0f));
-					break;
-				case "boolean":
-					isearcher.setSimilarity(new BooleanSimilarity());
-					break;
-				case "lmd":
-					isearcher.setSimilarity(new LMDirichletSimilarity());
-					break;
-				case "lmj":
-					isearcher.setSimilarity(new LMJelinekMercerSimilarity(0.7f));
-					break;
-				case "ibs":
-					isearcher.setSimilarity(new IBSimilarity(new DistributionLL(), new LambdaDF(), new NormalizationH1()));
-					break;
-				case "axiom":
-					isearcher.setSimilarity(new AxiomaticF2LOG());
-					break;
-				default:
-					break;
-			}
-			int queries_to_consider = 25; //queryList.size()
-			List<String> resultsFile = new ArrayList<String>();
-			for(int i=0;i<queries_to_consider;i++)
-			{
-				Map<String,String> query_collection = queryList.get(i);
-				//String queryNo = query_collection.get("query_no");
-    			//String description = query_collection.get("description");
-				//System.out.println(queryNo + " " + description);
-				MultiFieldQueryParser queryParser = new MultiFieldQueryParser(
-                        	//new String[]{"title","narrative", "description"},
-							new String[]{"headline","text","byLine"},
-                        	analyzer);
-				//only using description + narrative for start
-				String d = query_collection.get("description");
-				String escapedDescription = queryParser.escape(d);
-				String n = query_collection.get("narrative");
-				String escapedNarrative = queryParser.escape(n);
-				Query query = queryParser.parse(escapedDescription + " " + escapedNarrative);
-				ScoreDoc[] hits = isearcher.search(query, 1000).scoreDocs;
-				for(int j=0;j<hits.length;j++)
-				{
-					Document hitDoc = isearcher.doc(hits[j].doc);
-					resultsFile.add(query_collection.get("id")+ " Q0 "+ hitDoc.get("documentID") + " 0 " + hits[j].score + " STANDARD");
-				}
-			}
-			Files.write(Paths.get("./results.txt"),resultsFile,Charset.forName("UTF-8"));
+    public static void main(String[] args) throws IOException {
+        ProcessData readFile = new ProcessData();
+        List<Map<String, String>> queryList = readFile.readQueries();
+        LuceneSearch searcher = new LuceneSearch();
+        System.out.println(args[0] + " " + args[1]);
+        searcher.searchAndExtractFeatures(args[0], args[1], queryList);
+    }
 
-		}
-		catch(IOException | ParseException e)
-		{
-			e.printStackTrace();
-			System.exit(1);
-		}
+    public static void searchAndExtractFeatures(String analyzer_name, String similarity_strat, List<Map<String, String>> queryList) {
+        try {
+            Analyzer analyzer = null;
+            // Use the same analyzer as the CreateIndex class
+            switch (analyzer_name) {
+                case "standard":
+                    analyzer = new StandardAnalyzer(EnglishAnalyzer.getDefaultStopSet());
+                    break;
+                case "keyword":
+                    analyzer = new KeywordAnalyzer();
+                    break;
+                case "whitespace":
+                    analyzer = new WhitespaceAnalyzer();
+                    break;
+                case "simple":
+                    analyzer = new SimpleAnalyzer();
+                    break;
+                case "stop":
+                    analyzer = new StopAnalyzer();
+                    break;
+                case "english":
+                    analyzer = new EnglishAnalyzer();
+                    break;
+                default:
+                    analyzer = new StandardAnalyzer();
+                    break;
+            }
 
-	}
+            // Load indexes
+            Directory directory = FSDirectory.open(Paths.get("./index/"));
+            DirectoryReader ireader = DirectoryReader.open(directory);
+            IndexSearcher isearcher = new IndexSearcher(ireader);
+
+            // Depending on the system argument, choose the similarity score
+            switch (similarity_strat) {
+                case "classic":
+                    isearcher.setSimilarity(new ClassicSimilarity());
+                    break;
+                case "bm25":
+                    isearcher.setSimilarity(new BM25Similarity(1.9f, 1.0f));
+                    break;
+                case "boolean":
+                    isearcher.setSimilarity(new BooleanSimilarity());
+                    break;
+                case "lmd":
+                    isearcher.setSimilarity(new LMDirichletSimilarity());
+                    break;
+                case "lmj":
+                    isearcher.setSimilarity(new LMJelinekMercerSimilarity(0.7f));
+                    break;
+                case "ibs":
+                    isearcher.setSimilarity(new IBSimilarity(new DistributionLL(), new LambdaDF(), new NormalizationH1()));
+                    break;
+                case "axiom":
+                    isearcher.setSimilarity(new AxiomaticF2LOG());
+                    break;
+                default:
+                    break;
+            }
+
+            int queries_to_consider = 25; // queryList.size()
+            List<String> resultsFile = new ArrayList<>();
+
+            // Prepare feature file
+            FileWriter featureFile = new FileWriter("features.csv");
+            featureFile.write("query_id,doc_id,bm25_score,relevance\n");
+
+            for (int i = 0; i < queries_to_consider; i++) {
+                Map<String, String> query_collection = queryList.get(i);
+
+                MultiFieldQueryParser queryParser = new MultiFieldQueryParser(
+                        new String[]{"headline", "text", "byLine"},
+                        analyzer
+                );
+
+                // Only using description + narrative for start
+                String d = query_collection.get("description");
+                String escapedDescription = queryParser.escape(d);
+                String n = query_collection.get("narrative");
+                String escapedNarrative = queryParser.escape(n);
+                Query query = queryParser.parse(escapedDescription + " " + escapedNarrative);
+
+                ScoreDoc[] hits = isearcher.search(query, 1000).scoreDocs;
+
+                for (int j = 0; j < hits.length; j++) {
+                    Document hitDoc = isearcher.doc(hits[j].doc);
+                    String queryId = query_collection.get("id");
+                    String docId = hitDoc.get("documentID");
+                    float bm25Score = hits[j].score;
+
+                    // Placeholder for relevance (set to 0 for now)
+                    int relevance = 0; // Replace with actual relevance lookup logic if available
+
+                    // Add the feature row
+                    featureFile.write(queryId + "," + docId + "," + bm25Score + "," + relevance + "\n");
+
+                    // Also add to results.txt for standard output
+                    resultsFile.add(queryId + " Q0 " + docId + " 0 " + bm25Score + " STANDARD");
+                }
+            }
+
+            featureFile.close();
+            Files.write(Paths.get("./results.txt"), resultsFile, Charset.forName("UTF-8"));
+            System.out.println("Feature extraction and results file generation completed!");
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 }
